934c584ef769d46a558245945ef84c4c1eedca4e
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-11-03 12:33:57 -0300

    feat(mobile): render webview component

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
+import {WebView} from 'react-native-webview';
+
 import {Webstore} from '../sections/Webstore';
 
 export const Home = () => {
   return (
     <View style={styles.container}>
       <Webstore />
+      <WebView source={{uri: 'www.google.com'}} />
     </View>
   );
 };
