7ea3403e749251c7c46dcec36d3127bf468ffe80
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-11-07 13:54:47 -0300

    feat(web): add tailwind support

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>something here web</h1>
+      <h1 className="text-3xl text-blue-600 font-bold underline">
+        Something here
+      </h1>
     </>
   );
 }
