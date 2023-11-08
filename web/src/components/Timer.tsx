4b531e07c93361e7d0bed9bd52d1ddaa6ef897ea
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-11-08 12:46:16 -0300

    feat(web): add timer component

--- web/src/components/Timer.tsx
+++ web/src/components/Timer.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { usePayment } from '../context/PaymentProvider';
+import { addZero } from '../utils';
 
 export const Timer = () => {
+  const [state] = usePayment();
+
+  const [countdown, setCountdown] = useState(10);
+  const minutes = addZero(Math.floor(countdown / 60));
+  const seconds = addZero(Math.floor(countdown % 60));
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (countdown > 0) {
+        setCountdown((prev) => prev - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <div>
-      <div>20:04</div>
+    <div className="flex flex-col items-center">
+      <div className="font-bold text-4xl">
+        {minutes}:{seconds}
+      </div>
+      <span className="text-xs">Expiration time</span>
     </div>
   );
 };
--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,2 +1,6 @@
 export const sleep = (delay: number) =>
   new Promise((resolve) => setTimeout(resolve, delay));
+
+export const addZero = (value: number) => {
+  return String(value).padStart(2, '0');
+};
