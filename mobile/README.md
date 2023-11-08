7ab5093ee9d89eb18e49fec361f2877991d420f7
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-11-08 16:01:02 -0300

    feat(mobile): render localhost app inside webview

--- mobile/README.md
+++ mobile/README.md
@@ -5,5 +5,11 @@
 Start your native application
 
 ```sh
-   npx react-native run-android
+npx react-native run-android
+```
+
+Enable port to access local app:
+
+```sh
+adb reverse tcp:5173 tcp:5173
 ```
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import {WebViewNavigation} from 'react-native-webview';
 
 import {Webstore} from '../sections/Webstore';
 import {SDKWebView} from '../lib/SDKWebView';
 
 export const Home = () => {
+  const handleNavigationChange = (navState: WebViewNavigation) => {
+    const {url, title} = navState;
+    console.log(`[inapp-sdk] - navigation change: ${url}-${title}`);
+  };
+
   return (
     <View style={styles.container}>
       <Webstore />
-      <SDKWebView source={{uri: 'www.google.com'}} />
+      <SDKWebView
+        source={{uri: 'http://localhost:5173'}}
+        javaScriptEnabled={true}
+        onNavigationStateChange={handleNavigationChange}
+      />
     </View>
   );
 };
